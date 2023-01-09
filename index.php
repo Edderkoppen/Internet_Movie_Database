798c5f3b281cf82daafa53919e1c896b521a32cf
Mathieu <cartron.mathieu37@gmail.com>
2023-01-09 22:47:00 +0100

    Update: Création de la BDD + récupération d'une donnée avec php

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
 
+try {
+    $bdd = new PDO("mysql:host=localhost; dbname=internetmovies; charset=utf8", "root", "", array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+
+} catch (Exception $e) {
+
+    die("Erreur : " . $e->getMessage());
+}
+
+$req=$bdd->prepare("insert into CATEGORIE VALUES(1, 'Drame' )");
+$req->execute();
+$req2 = $bdd->prepare("select * from categorie");
+$req2->execute();
+$test = $req2->fetchAll();
+
+foreach ($test as $test) {
+
+?>
+
+    <p> <?php echo $test['nom_categorie']; ?> </p>
+
+<?php
+}
+
 echo "test Ryan";
 
+?>
 
