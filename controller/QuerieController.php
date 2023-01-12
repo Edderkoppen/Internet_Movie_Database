956adc530048142492395dfa39344323019e4b30
Mathieu <cartron.mathieu37@gmail.com>
2023-01-12 23:16:46 +0100

    Update : POO pour les pages

--- controller/QuerieController.php
+++ controller/QuerieController.php
@@ -5,8 +5,34 @@ namespace controller;
 class QuerieController
 {
     public function __construct(){}
-    public function selectQuerie($coucou){
-        echo $coucou;
+    public function selectQuerie($data, $from){
+        return "select " . $data . " from " . $from . ";";
     }
 
-}
+    public function test($data){ ?>
+        <section id="last">
+        <h2 class="titre">Derniers films</h2>
+        <a class="plus" href="#"> Voir plus</a>
+        <ul class="alignement">
+
+        <?php foreach ($data as $donnee) { ?>
+            <li class="item-1">
+                <div class="last-box">
+                    <div class="last-img-1">
+                        <div class="qualite">HDRip</div>
+                        <?php echo "<img src=\"../" . $donnee['image_path'] . "\" alt=\"affiche " . $donnee["titre"] . "\" >"; ?>
+                    </div>
+
+                    <div class="last-txt-1">
+                        <strong><?php echo $donnee['titre']; ?></strong>
+                        <p><?php echo $donnee['prix'] . "€"; ?></p>
+                    </div>
+                </div>
+            </li>
+        <?php } ?>
+
+        </ul>
+    </section>
+    <?php }
+
+} ?>
--- layout/accueil.php
+++ layout/accueil.php
@@ -1,52 +1,26 @@
 <html>
-<head>
-    <title> InternetMovies </title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="../css/styles.css"/>
-    <link rel="stylesheet" href="fontawesome.min.css"/>
-    <link rel="shortcut icon" href="../images/logo.png"/>
-    <script src="https://kit.fontawesome.com/c8e4d183c2.js" crossorigin="anonymous"></script>
-    <script src="https://example.com/fontawesome/v5.15.4/js/all.js" data-auto-replace-svg></script>
-</head>
+
+<?php include_once "../views/meta.html"; ?>
+
 <body>
+
     <?php
-        include_once "../views/header.html";
-        include_once "../controller/QuerieController.php";
-        include_once("../model/connection.php");
-        use controller\QuerieController;
+    include_once "../views/header.html";
+    include_once "../controller/QuerieController.php";
+    include_once "../model/connection.php";
 
-        $test = new QuerieController("test");
-        $test->selectQuerie("coucouqlisdfh");
-        $requete = $bdd->prepare("select * from FILM");
-        $requete->execute();
-        $donnee = $requete->fetchAll();
+    use controller\QuerieController;
+
+    $querie = new QuerieController();
+
+    $requete = $bdd->prepare($querie->selectQuerie("*", "FILM"));
+    $requete->execute();
+
+    $donnee = $requete->fetchAll();
+    $querie->test($donnee);
+
+    include_once "../views/footer.html";
     ?>
-
-    <section id="last">
-        <h2 class="titre">Derniers films</h2>
-        <a class="plus" href="#"> Voir plus</a>
-        <ul class="alignement">
-
-        <?php foreach ($donnee as $donnee) { ?>
-            <li class="item-1">
-                <div class="last-box">
-                    <div class="last-img-1">
-                        <div class="qualite">HDRip</div>
-                        <?php echo "<img src=\"../" . $donnee['image_path'] . "\" alt=\"affiche " . $donnee["titre"] . "\" >"; ?>
-                    </div>
-
-                    <div class="last-txt-1">
-                        <strong><?php echo $donnee['titre']; ?></strong>
-                        <p><?php echo $donnee['prix'] . "€"; ?></p>
-                    </div>
-                </div>
-            </li>
-        <?php } ?>
-
-        </ul>
-    </section>
-
-    <?php include_once "../views/footer.html"; ?>
 
 </body>
 </html>
